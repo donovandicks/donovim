8794edca02678833c264aef2212b0d2848139fec
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-06 00:21:50 -0500

    Reduce clippy leniency

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,4 @@
-#![allow(
-    clippy::missing_docs_in_private_items,
-    clippy::implicit_return,
-    clippy::shadow_reuse,
-    clippy::print_stdout,
-    clippy::wildcard_enum_match_arm,
-    clippy::else_if_without_else,
-)]
+#![allow(clippy::print_stdout)]
 use donovim::Editor;
 
 /**

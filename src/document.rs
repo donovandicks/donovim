a332ea6abe891a5a7286618574dc653c057d20f9
Donovan Dicks <donovan.dicks@outlook.com>
2021-12-06 01:04:22 -0500

    Begin cleanup

--- src/document.rs
+++ src/document.rs
@@ -11,16 +11,20 @@ pub struct Document {
 }
 
 impl Document {
-    /**
-     * Open a file and store the contents in rows vector
-     */
+    /// Open a file and store the contents in the `rows` vector
+    ///
+    /// # Args
+    ///
+    /// - `filename`: The plain name of the file to open
+    ///
+    /// # Returns
+    ///
+    /// - The `Document` if successful
     pub fn open(filename: &str) -> Result<Self, std::io::Error> {
-        let contents: String = fs::read_to_string(filename)?;
-        let file_type: FileType = FileType::from(filename);
-        let mut rows: Vec<Row> = Vec::new();
-        for line in contents.lines() {
-            rows.push(Row::from(line));
-        }
+        let contents = fs::read_to_string(filename)?;
+        let file_type = FileType::from(filename);
+        let rows = contents.lines().map(Row::from).collect();
+
         Ok(Self {
             rows,
             file_name: Some(filename.to_string()),
@@ -29,85 +33,107 @@ impl Document {
         })
     }
 
+    /// Retrieve the file type of the current `Document`
+    ///
+    /// # Returns
+    ///
+    /// - The name of the filetype
     pub fn file_type(&self) -> String {
         self.file_type.name()
     }
 
-    /**
-     * Write document to disk
-     */
+    /// Write the current `Document` to disk
     pub fn save(&mut self) -> Result<(), Error> {
         if let Some(file_name) = &self.file_name {
-            let mut file: fs::File = fs::File::create(file_name)?;
+            let mut file = fs::File::create(file_name)?;
             self.file_type = FileType::from(file_name);
-            for row in &mut self.rows {
+
+            for row in &self.rows {
                 file.write_all(row.as_bytes())?;
                 file.write_all(b"\n")?;
             }
             self.dirty = false;
         }
+
         Ok(())
     }
 
-    /**
-     * Insert a single character into a Document at a given position
-     */
+    /// Insert a single character into a Document at a given position
+    ///
+    /// # Args
+    ///
+    /// - `at`: The (x, y) pair where the character should be placed
+    /// - `c`: The character to insert
     pub fn insert(&mut self, at: &Position, c: char) {
         if at.y > self.len() {
             return;
         }
+
         self.dirty = true;
         if c == '\n' {
             self.insert_newline(at);
         } else if at.y == self.rows.len() {
-            let mut row: Row = Row::default();
+            let mut row = Row::default();
             row.insert(0, c);
             self.rows.push(row);
         } else {
-            let row: &mut Row = self.rows.get_mut(at.y).unwrap();
+            let row = self.rows.get_mut(at.y).unwrap();
             row.insert(at.x, c);
         }
+
         self.unhighlight_rows(at.y);
     }
 
-    /**
-     * Adds a line, moving the remainder of a current line down if applicable
-     */
+    /// Adds a line, moving the remainder of a current line down if applicable
+    ///
+    /// # Args
+    ///
+    /// - `at`: The (x, y) pair where the new newline should be placed
     fn insert_newline(&mut self, at: &Position) {
         if at.y == self.len() {
             self.rows.push(Row::default());
             return;
         }
-        let current_row: &mut Row = &mut self.rows[at.y];
-        let new_row: Row = current_row.split(at.x);
+
+        let current_row = &mut self.rows[at.y];
+        let new_row = current_row.split(at.x);
         self.rows.insert(at.y + 1, new_row);
     }
 
-    /**
-     * Remove the character under the cursor
-     */
+    /// Remove the character under the cursor
     pub fn delete(&mut self, at: &Position) {
         let len: usize = self.len();
+
         if at.y >= len {
             return;
         }
+
         self.dirty = true;
+
         if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
-            let next_row: Row = self.rows.remove(at.y + 1);
-            let row: &mut Row = self.rows.get_mut(at.y).unwrap();
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
             row.append(&next_row);
         } else {
-            let row: &mut Row = self.rows.get_mut(at.y).unwrap();
+            let row = self.rows.get_mut(at.y).unwrap();
             row.delete(at.x);
         }
+
         self.unhighlight_rows(at.y);
     }
 
-    /**
-     * Search document for query
-     */
+    /// Search document for query
+    ///
+    /// # Args
+    ///
+    /// - `query`: The text to search for
+    /// - `after`: The starting location to search from
+    ///
+    /// # Returns
+    ///
+    /// - The position of the query if found
     pub fn find(&self, query: &str, after: &Position) -> Option<Position> {
-        let mut x: usize = after.x;
+        let mut x = after.x;
         for (y, row) in self.rows.iter().enumerate().skip(after.y) {
             if let Some(x) = row.find(query, x) {
                 return Some(Position { x, y });
@@ -118,26 +144,34 @@ impl Document {
     }
 
     pub fn find_all(&self, query: &str) -> Vec<Position> {
-        let mut results: Vec<Position> = Vec::new();
+        // TODO: Refactor
+        let mut results = Vec::new();
+
         for (y, row) in self.rows.iter().enumerate() {
             if let Some(x) = row.find(query, 0) {
                 results.push(Position { x, y });
             }
         }
+
         results
+    }
+
+    fn parse_until(&mut self, until: Option<usize>) -> usize {
+        match until {
+            Some(u) => {
+                if u.saturating_add(1) < self.rows.len() {
+                    u.saturating_add(1)
+                } else {
+                    self.rows.len()
+                }
+            }
+            None => self.rows.len(),
+        }
     }
 
     pub fn highlight(&mut self, word: &Option<String>, until: Option<usize>) {
         let mut start_with_comment: bool = false;
-        let until = if let Some(until) = until {
-            if until.saturating_add(1) < self.rows.len() {
-                until.saturating_add(1)
-            } else {
-                self.rows.len()
-            }
-        } else {
-            self.rows.len()
-        };
+        let until = self.parse_until(until);
 
         for row in &mut self.rows[..until] {
             start_with_comment = row.highlight(
@@ -149,7 +183,7 @@ impl Document {
     }
 
     fn unhighlight_rows(&mut self, start: usize) {
-        let start: usize = start.saturating_sub(1);
+        let start = start.saturating_sub(1);
         for row in self.rows.iter_mut().skip(start) {
             row.is_highlighted = false;
         }
